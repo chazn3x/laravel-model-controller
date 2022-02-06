d7cb990ab70346802bf92ae4475ad845b5655a62
Giovanni <gio.a.98@gmail.com>
2022-02-06 13:14:56 +0100

    fine esercizio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,28 @@
-@dump($movies)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movies</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <header>
+        <h1>Movies</h1>
+    </header>
+    <main class="container">
+        <div class="movies">
+            @foreach ($movies as $movie)
+                <div class="movie">
+                    <h2>{{$movie->title}}</h2>
+                    <h3>Titolo originale: {{$movie->original_title}}</h3>
+                    <p>Produzione: {{$movie->nationality}}</p>
+                    <p>Data di uscita: {{$movie->date}}</p>
+                    <p>Voto: {{$movie->vote}}</p>
+                </div>
+            @endforeach
+        </div>
+    </main>
+</body>
+</html>
